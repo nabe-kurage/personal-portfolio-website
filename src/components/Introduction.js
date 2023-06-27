5b89a6ea0436a6a17789c819055ae330f43376fc
nabe-kurage <tabiflag@gmail.com>
2023-06-28 00:17:25 +0900

    fix files

---
+++ src/components/Introduction.js
@@ -0,0 +1,9 @@
+function Introduction() {
+  return (
+    <section className="section">
+      <h2>Introduction</h2>
+    </section>
+  );
+}
+
+export default Introduction;
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <h1>
+        <a href="">Nabekura</a>
+      </h1>
+    </nav>
+  );
+}
+
+export default Navigation;
