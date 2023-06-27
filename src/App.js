f27e33bf875330ad565b4a64e2e54063c12793a6
nabe-kurage <tabiflag@gmail.com>
2023-06-27 22:58:19 +0900

    add minimam files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./reset.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <nav className="navigation">
+                <h1>
+                    <a href="">Nabekura</a>
+                </h1>
+            </nav>
+
+            <section className="section">
+                <h2>Top</h2>
+            </section>
+
+            <section className="section">
+                <h2>Goal</h2>
+            </section>
+
+            <section className="section">
+                <h2>Concept</h2>
+            </section>
+
+            <section className="section">
+                <h2>Trajectory</h2>
+            </section>
+
+            <section className="section">
+                <h2>Travel</h2>
+            </section>
+
+            <section className="section">
+                <h2>Links</h2>
+            </section>
+
+            <footer className="footer">
+                <h2>footer</h2>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
