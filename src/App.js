b1fd646bd832b4a1f50cce5fefb47f81294f85e1
nabe-kurage <tabiflag@gmail.com>
2023-06-27 23:53:17 +0900

    fix indnet

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import "./App.css";
-import "./reset.css";
+import './App.css';
+import './reset.css';
 
 function App() {
-    return (
-        <div className="wrapper">
-            <nav className="navigation">
-                <h1>
-                    <a href="">Nabekura</a>
-                </h1>
-            </nav>
+  return (
+    <div className="wrapper">
+      <nav className="navigation">
+        <h1>
+          <a href="">Nabekura</a>
+        </h1>
+      </nav>
 
-            <section className="section">
-                <h2>Top</h2>
-            </section>
+      <section className="section">
+        <h2>Top</h2>
+      </section>
 
-            <section className="section">
-                <h2>Goal</h2>
-            </section>
+      <section className="section">
+        <h2>Goal</h2>
+      </section>
 
-            <section className="section">
-                <h2>Concept</h2>
-            </section>
+      <section className="section">
+        <h2>Concept</h2>
+      </section>
 
-            <section className="section">
-                <h2>Trajectory</h2>
-            </section>
+      <section className="section">
+        <h2>Trajectory</h2>
+      </section>
 
-            <section className="section">
-                <h2>Travel</h2>
-            </section>
+      <section className="section">
+        <h2>Travel</h2>
+      </section>
 
-            <section className="section">
-                <h2>Links</h2>
-            </section>
+      <section className="section">
+        <h2>Links</h2>
+      </section>
 
-            <footer className="footer">
-                <h2>footer</h2>
-            </footer>
-        </div>
-    );
+      <footer className="footer">
+        <h2>footer</h2>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
